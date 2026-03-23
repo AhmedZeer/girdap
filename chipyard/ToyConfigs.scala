01c95bab3dff4df4513b692b39d55e986587f880
AhmetZeer <ahmed.zeer@std.yildiz.edu.tr>
2026-03-23 18:32:58 +0300

    mae cong

--- chipyard/ToyConfigs.scala
+++ chipyard/ToyConfigs.scala
@@ -1,10 +1,9 @@
 package chipyard
 
 import org.chipsalliance.cde.config.Config
-import toyrocc.WithToyRoCC
 
 class DummyToyRoCCConfig extends Config(
-  new WithToyRoCC ++
+  new toyrocc.WithToyRoCC ++
   new freechips.rocketchip.rocket.WithNHugeCores(1) ++
   new chipyard.config.WithSystemBusWidth(128) ++
   new chipyard.config.AbstractConfig
