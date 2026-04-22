7dfa2828789f8d6fc5cb652b20de5ec1da0c1d79
AhmetZeer <ahmed.zeer@std.yildiz.edu.tr>
2026-04-22 11:02:50 +0300

    2x2 sys

--- chipyard/ToyConfigs.scala
+++ chipyard/ToyConfigs.scala
@@ -1,7 +1,7 @@
 package chipyard
 
 import org.chipsalliance.cde.config.Config
-import toyrocc.{AttnAccel4x4, MatmulAccel2x2, MatmulAccel2x2WS, MatmulAccel4x4, MatmulAccel4x4WS, MatmulAccel4x4WSBF16, SoftmaxAccel}
+import toyrocc.{AttnAccel4x4, MatmulAccel2x2, MatmulAccel2x2WS, MatmulAccel2x2WSBF16, MatmulAccel4x4, MatmulAccel4x4WS, MatmulAccel4x4WSBF16, SoftmaxAccel}
 
 class AttnAccel4x4WS256Config extends Config(
   new AttnAccel4x4 ++
